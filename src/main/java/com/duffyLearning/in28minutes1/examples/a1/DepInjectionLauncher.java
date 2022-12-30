ef575de1b32f1edee65156b49a67975cf9713470
Christopher-Duffy <77360354+ChrisBDuffy@users.noreply.github.com>
2022-12-29 21:05:06 -0800

    Formatting toString

--- src/main/java/com/duffyLearning/in28minutes1/examples/a1/DepInjectionLauncher.java
+++ src/main/java/com/duffyLearning/in28minutes1/examples/a1/DepInjectionLauncher.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Component;
 @Component
 class BusinessClass{
 	
+
+
 	Dependency1 dependency1;
 	Dependency2 dependency2;
 	
@@ -18,18 +20,20 @@ class BusinessClass{
 		this.dependency1 = dependency1;
 		this.dependency2 = dependency2;
 	}
-	@Autowired
+	
 	public void setDependency1(Dependency1 dependency1) {
 		System.out.println("setdependency1");
 		this.dependency1 = dependency1;
 	}
-	@Autowired
+	
 	public void setDependency2(Dependency2 dependency2) {
 		System.out.println("setdependency2");
 		this.dependency2 = dependency2;
 	}
+	
+	@Override
 	public String toString() {
-		return "using " + dependency2 + " and " + dependency2;
+		return "BusinessClass [dependency1=" + dependency1 + ", dependency2=" + dependency2 + "]";
 	}
 }
 @Component
